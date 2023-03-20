2b46e298b8cc146236425f253da5ff52fad2c8b5
Melk de Sousa <melk.sousap@gmail.com>
2023-03-19 22:15:45 -0300

    test with react-native-esc-pos-printer

--- App.tsx
+++ App.tsx
@@ -1,30 +1,60 @@
 import { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Switch, Text, View } from 'react-native'
-import { bleManager } from './src/adapters/blue-manager'
+import EscPosPrinter, { IPrinter } from 'react-native-esc-pos-printer'
+
+type Device = {
+  name: string
+  ip: string
+  mac: string
+}
+
+const toDevice = (rawPrinter: IPrinter): Device => ({
+  ip: rawPrinter.ip,
+  mac: rawPrinter.mac,
+  name: rawPrinter.name,
+})
 
 export default function App() {
   const [scanning, setScanning] = useState(false)
+  const [devices, setDevices] = useState<Device[]>([])
+
+  const hasDevices = devices.length > 0
+
+  const scan = async () => {
+    EscPosPrinter.discover()
+      .then((printers) => setDevices(printers.map(toDevice)))
+      .catch((e) => console.error(e))
+      .finally(() => setScanning(false))
+  }
 
   useEffect(() => {
     if (scanning) {
-      bleManager.scan()
-    }
-
-    if (!scanning) {
-      bleManager.stopScan()
+      scan()
     }
   }, [scanning])
 
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
       <Switch value={scanning} onValueChange={setScanning} />
 
       {scanning && <ActivityIndicator />}
 
-      {!scanning && (
+      {!hasDevices && (
         <FlatList
-          data={bleManager.devices}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
+          data={devices}
+          renderItem={({ item }) => (
+            <Text>
+              {item.mac} - {item.name}
+            </Text>
+          )}
         />
       )}
     </View>
